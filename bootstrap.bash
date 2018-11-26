7f3bb31391b0777f87f7f4fd0513c5bed883b44d
Eetu Sjöblom <eetu.sjoblom@helsinki.fi>
2018-11-26 09:42:54 +0200

    modify bootstrap

--- bootstrap.bash
+++ bootstrap.bash
@@ -8,3 +8,11 @@ rm bootstrap.bash
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 git clone https://github.com/Eetusjo/computational-semantics.git
+
+cd computational-semantics/assignment5/data/quora/
+gunzip quora_duplicate_questions.tsv.gz 
+
+cd /home/jovyan/
+
+pip install spacy
+python -m spacy download en
