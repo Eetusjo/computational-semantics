2f31d8f59293f037f659eab60571af96e23e365e
Eetu Sjöblom <eetu.sjoblom@helsinki.fi>
2018-11-26 09:21:22 +0200

    bootstrap

---
+++ bootstrap.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /home/jovyan/
+
+rm -r work
+rm bootstrap.bash
+
+export GIT_COMMITTER_NAME=anonymous
+export GIT_COMMITTER_EMAIL=anon@localhost
+git clone https://github.com/Eetusjo/nbsandbox.git
