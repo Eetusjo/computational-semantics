d61ae8be6ae522f1c1015910be8d2645a873385d
Eetu Sjöblom <eetu.sjoblom@helsinki.fi>
2018-11-26 09:59:55 +0200

    test modified bootstrap because of 502 error

--- bootstrap.bash
+++ bootstrap.bash
@@ -9,10 +9,10 @@ export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 git clone https://github.com/Eetusjo/computational-semantics.git
 
-cd computational-semantics/assignment5/data/quora/
-gunzip quora_duplicate_questions.tsv.gz 
+# cd computational-semantics/assignment5/data/quora/
+# gunzip quora_duplicate_questions.tsv.gz 
 
-cd /home/jovyan/
+# cd /home/jovyan/
 
 pip install spacy
 python -m spacy download en
